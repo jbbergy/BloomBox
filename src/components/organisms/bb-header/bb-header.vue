ce9c6c8404ce47f4e074c78bf040d2846988e6d7
Jibhey <47273038+jbbergy@users.noreply.github.com>
2023-03-01 21:03:26 +0100

    remove useless header logo

--- src/components/organisms/bb-header/bb-header.vue
+++ src/components/organisms/bb-header/bb-header.vue
@@ -1,25 +1,3 @@
 <template>
-  <div class="bb-header">
-      <inline-svg
-        :src="IconLogo"
-        aria-label="Logo Bloombox"
-      />
-  </div>
+  <div class="bb-header"/>
 </template>
-
-<script lang="ts" setup>
-import InlineSvg from 'vue-inline-svg'
-import IconLogo from '../../../assets/img/logo.svg'
-
-</script>
-
-<style lang="scss">
-.bb-header {
-  padding: $bb-spacing-small;
-
-  svg {
-    height: 2rem;
-    width: 7rem;
-  }
-}
-</style>
