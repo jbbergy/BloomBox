071e0ac12ad37ece15760f27fb6dd6cbd1412cfc
Jibhey <47273038+jbbergy@users.noreply.github.com>
2023-05-26 01:09:09 +0200

    recursive random value method

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,14 +1,16 @@
-let previous: number | null = null
-export const getRandomValue = (lower: number, upper: number) => {
-  if ((previous || 0) > upper) previous = null
+let previousValue: number | null = null
+let newValue = 0
+
+export const getRandomValue = (lowerLimit: number, upperLimit: number): number => {
+  if ((previousValue ?? 0) > upperLimit) previousValue = null
   const crypto = window.crypto
-  const range = upper - lower + 1
-  const array = new Uint32Array(1)
-  crypto.getRandomValues(array)
-  let result = lower + (array[0] % range)
-  if (previous === result) {
-    result = getRandomValue(lower, upper)
+  const range = upperLimit - lowerLimit + 1
+  const arrayValues = new Uint32Array(1)
+  crypto.getRandomValues(arrayValues)
+  newValue = lowerLimit + (arrayValues[0] % range)
+  if (previousValue === newValue) {
+    return getRandomValue(lowerLimit, upperLimit)
   }
-  previous = result
-  return result
+  previousValue = newValue
+  return newValue
 }
